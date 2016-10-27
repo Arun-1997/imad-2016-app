5e6de298f151348134e630aadc6e4975beb1d9c6
Arun-1997 <aarun.vishwanath@gmail.com>
2016-10-27 23:35:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var main= require('ui/main');
+
 var app = express();
 app.use(morgan('combined'));
 
