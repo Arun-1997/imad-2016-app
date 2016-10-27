0c1a0dc31a6027682d778a4818645ebf167d3e08
Arun-1997 <aarun.vishwanath@gmail.com>
2016-10-27 23:43:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,19 @@
-console.log("Loaded");
+var pos = 0;
+    
+function gotoimg1()
+{
+    var x = document.getElementById('img1');
+    var t = setInterval(move,15);
+    function move()
+    {
+        if(pos<600)
+        {
+            x.style.left = pos+"px";
+            pos+=1;
+        }
+        else
+        {
+            clearInterval(t);
+        }
+    }
+}
